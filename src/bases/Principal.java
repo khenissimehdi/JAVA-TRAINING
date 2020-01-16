a92468cec1cf9a30813a5e9d8d9e9373baabd7c8
Mehdi KHENISSI <mehdi.khenissi@etudiant.univ-reims.fr>
2020-01-16 09:51:49 +0100

    Third Commit

--- src/bases/Principal.java
+++ src/bases/Principal.java
@@ -1,8 +1,14 @@
 package bases;
 
 public class Principal {
+	
 	public static void main(String[] args ) 
 	{
+		String prenom = "charles";
+		int longueur = prenom.length();
+		System.out.print(prenom);
+		System.out.print(" (" + longueur +" caracteres)");
+		
 		
 	}
 
