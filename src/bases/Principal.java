b9efe6ad9a1eb6ad6b1f187eca4a3834d798230d
Mehdi KHENISSI <mehdi.khenissi@etudiant.univ-reims.fr>
2020-01-16 12:04:15 +0100

    Fin Palindrome (manque test)

--- src/bases/Principal.java
+++ src/bases/Principal.java
@@ -5,18 +5,19 @@ public class Principal
 	
 	public static void main(String[] args )
 	{
-		
-		System.out.print( Principal.estUnPalindrome("abba") );	
+		Principal.recherche("bonjour le chat et le chien", "ch");
 		
 	}
 	
+	/** EXERCICE 1 **/
 	
 	/**
 	 * Retourne une description de la longueur d'une chaine.
 	 * @param chaine
 	 * @return Longueur de la chaine 
 	 */
-	public static String longueurDeChaine(String chaine) {
+	public static String longueurDeChaine(String chaine)
+	{
 		return chaine.length() + " caractères";
 	}
 	
@@ -26,7 +27,8 @@ public class Principal
 	 * @param secondInt
 	 * @return Valeur max
 	 */
-	public static int maximum(int firstInt,int secondInt) {
+	public static int maximum(int firstInt,int secondInt)
+	{
 		int res = (firstInt < secondInt) ? secondInt : firstInt;
 		return res ;
 	}
@@ -37,7 +39,8 @@ public class Principal
 	 * @param carac
 	 * @return haine de caractères
 	 */
-	public static String masquerLettres(String chaine,char carac) {
+	public static String masquerLettres(String chaine,char carac) 
+	{
 		String res = "";
 		for(int i = 0 ; i < chaine.length();i++) {
 			res += carac;
@@ -45,6 +48,11 @@ public class Principal
 		return res;
 	}
 
+	/**
+	 * Retourne True si la chaine en paramètre est un palindrome, False dans le cas contraire.
+	 * @param chaine
+	 * @return 
+	 */
 	public static boolean estUnPalindrome(String chaine)
 	{
 		boolean res = true;
@@ -64,6 +72,31 @@ public class Principal
 		}
 		return res ;
 		
+	}
+	
+	
+	
+	
+	/** EXERCICE 2 **/
+	
+	
+	public static int recherche(String chaine, String chaineRecherche)
+	{
+		int res = -1;
+		int longueurChaine = chaine.length();
+		int longueurRecherche = chaineRecherche.length();
+		int i = 0;
+		
+		while (i < longueurChaine-longueurRecherche && res == -1)
+		{
+			if(chaineRecherche.equals(chaine.substring(i,i+longueurRecherche)))
+			{
+				res = i;
+			}
+			i++;
+		}
+		
+		return res;
 	}
 	
 }
