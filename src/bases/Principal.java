22773aec387411d19ace48ae8e5f89db25e3b980
Mehdi KHENISSI <mehdi.khenissi@etudiant.univ-reims.fr>
2020-01-16 10:41:45 +0100

    Début Test Unitaire (exercice 1)

--- src/bases/Principal.java
+++ src/bases/Principal.java
@@ -11,8 +11,19 @@ public class Principal
 		System.out.print(" (" + Principal.longueurDeChaine(prenom) +" caractères)");	
 	}
 	
+	
+	/**
+	 * Retourne une description de la longueur d'une chaine.
+	 * @param chaine
+	 * @return longueur de la chaine 
+	 */
 	public static String longueurDeChaine(String chaine) {
 		return chaine.length() + " caractères";
+	}
+	
+	public static int maximum(int firstInt,int secondInt) {
+		int res = (firstInt < secondInt) ? secondInt : firstInt;
+		return res ;
 	}
 
 }
---
+++ test/bases/PrincipalTest.java
@@ -0,0 +1,22 @@
+package bases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PrincipalTest {
+
+	@Test public void maximum_equal() {
+		assertEquals(1,Principal.maximum(1,1));
+	}
+
+	@Test public void maximum_firstInt() {
+		assertEquals(3,Principal.maximum(3,1));
+	}
+	
+	@Test public void maximum_secondInt() {
+		assertEquals(3,Principal.maximum(1,3));
+	}
+	
+	
+}
